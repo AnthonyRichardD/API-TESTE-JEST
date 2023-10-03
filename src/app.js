1de6ef225c334b5669c0972b674c9925a0cf2616
AnthonyRichardD <anthonyricharddasilvamartins2@gmail.com>
2023-10-03 11:13:39 -0300

    adicinando funções assíncronas.

--- src/app.js
+++ src/app.js
@@ -1,31 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
-const { exec } = require('child_process');
 const app = express();
 const port = 3000;
 
-function extrairNomeDaFuncao(definicaoDaFuncao) {
-
-    const regex = /function\s+([A-Za-z_$][A-Za-z0-9_$]*)\s*\(/;
-    const match = definicaoDaFuncao.match(regex);
-
-    if (match && match.length >= 2) {
-        return (`${definicaoDaFuncao}\n\nmodule.exports = ${match[1]}`)
-    } else {
-        return null;
-    }
-}
-
-function apagarArquivoTemporario(path) {
-    fs.unlink(path, (err) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log('Arquivo apagado com sucesso');
-        }
-    });
-}
+const extrairNomeDaFuncaoAdicionarExport = require('./functions/extrairNomeDaFuncaoAdicionarExport');
+const writeFile = require('./functions/writeFile');
+const executarTeste = require('./functions/executarTeste');
 
 const corsOptions = {
     origin: 'http://localhost:5173',
@@ -36,55 +16,39 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.post('/criar', (req, res) => {
-    const { stringParam, testPath } = req.body; // Suponha que você está enviando a string no corpo da solicitação JSON
+app.post('/criar', async (req, res) => {
+    const { stringParam, testPath } = req.body;
 
     const jestCommand = `npx jest --testPathPattern=/Subprogramas/${testPath}`;
-
     const tempFilePath = `./src/temp/arquivo_temporario.js`;
-
-    const scriptModule = extrairNomeDaFuncao(stringParam);
+    const scriptModule = extrairNomeDaFuncaoAdicionarExport(stringParam);
 
     if (scriptModule != null) {
-        fs.writeFile(tempFilePath, scriptModule, (err) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({ message: 'Erro ao criar o arquivo' });
-            } else {
-                console.log('Arquivo criado com sucesso');
-
-                // Executar o comando 'npm test'
-                setTimeout(() => {
-                    console.log("iniciando teste")
-                    exec(jestCommand, (error, stdout, stderr) => {
-                        if (error) {
-                            // O comando falhou
-                            console.log("Codigo não passou nos testes :(")
-                            apagarArquivoTemporario(tempFilePath);
-                            res.status(500).json({ message: stderr });
-                            console.log(error)
-                        } else {
-                            // O comando foi bem-sucedido
-                            apagarArquivoTemporario(tempFilePath);
-                            console.log(`Enviando ${stringParam} para o banco de dados :)`)
-                            res.status(200).json({ message: "Seu codigo passou nos testes realizados." });
-                        }
-                    });    
-                }, 5000);
-                
-            }
-        });
-
-
-
-
-
+        try {
+            await writeFile(tempFilePath, scriptModule);
+            
+            console.log("Iniciando teste");
+            const result = await executarTeste(jestCommand, tempFilePath);
+          
+            console.log(`Está correto?: ${result.isCorreto}`)
+            
+            res.status(200).json({ message: result });
+        } catch (result) {
+            console.log(`Saida do console: ${result.saida}`)
+            const expectedAndRecived = await sirius(result.saida);
+            res.status(500).json({ message: 'Erro ao executar o teste', saida: expectedAndRecived });
+        }
+    } else {
+        res.status(400).json({ message: 'Definição de função inválida' });
     }
-
-    // console.log("requisição recebida: " + stringParam)
-
 });
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
+
+async function sirius(text){
+    const filteredLines = text.match(/(Expected:|Received:)\s+\d+/g);
+
+    return ({expected: filteredLines[0], recived: filteredLines[1]})
+}
